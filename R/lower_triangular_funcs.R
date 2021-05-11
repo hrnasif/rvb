590637d50e3387730548f6127974b28329e3ca3b
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-05-10 17:48:30 -0700

    Update lower triangular notation

--- R/lower_triangular_funcs.R
+++ R/lower_triangular_funcs.R
@@ -15,14 +15,14 @@ get_lower_tri_vector <- function(W){
 #'
 #' Construct a lower triangular matrix from a vector of entries
 #'
-#' @param n Integer. Size of the output matrix
-#' @param omega Numeric vector of size n(n+1)/2. Entries to make matrix.
+#' @param r Integer. Size of the output matrix
+#' @param omega Numeric vector of size r(r+1)/2. Entries to make matrix.
 #'
-#' @return An n x n lower triangular matrix with `omega` entries column-wise
+#' @return An r x r lower triangular matrix with `omega` entries column-wise
 #'
 #' @export
-make_lower_tri_matrix <- function(n,omega){
-  W = matrix(0, nrow = n, ncol = n)
+make_lower_tri_matrix <- function(r,omega){
+  W <- matrix(0, nrow = r, ncol = r)
   W[lower.tri(W,diag = T)] = omega
   return(W)
 }
@@ -30,11 +30,11 @@ make_lower_tri_matrix <- function(n,omega){
 
 #' Indices of diagonal elements when in lower triangular vector
 #'
-#' @param n An integer. Dimenson of matrix
+#' @param r An integer. Dimension of matrix
 #'
 #' @keywords internal
-.diag_locs <- function(n){
-  c(1, cumsum(n:2)+1)
+.diag_locs <- function(r){
+  c(1, cumsum(r:2)+1)
 }
 
 
