06c6993af4718a7494a7b46bba72e39a7977d9cb
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-05-23 01:41:41 -0700

    ELBO estimate for RVB1

---
+++ R/LB_RVB1.R
@@ -0,0 +1,11 @@
+LB_RVB1 <- function(mu, C, y, X, Z, Zg, ZH, vbeta0, Sinv, model, m, n, p, r, d,
+                    N = 1000){
+  LB = 0
+  for (i in 1:N){
+    s = stats::rnorm(d)
+    ttheta = mu + C*s
+    LB = LB + logpy_RVB1(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, m, n, p, r) +
+      sum(log(diag(C))) + 0.5*crossprod(s)/N
+  }
+  return(LB)
+}
