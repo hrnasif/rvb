9f2c620e5c2e8b8e03cf7d4e63f6feea0a430884
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-05-24 23:19:32 -0700

    Fix bugs with iteration and r > 1 in RVB1

--- R/Lgrad_RVB1.R
+++ R/Lgrad_RVB1.R
@@ -3,11 +3,11 @@ Lgrad_RVB1 <- function(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, n, p, r, d)
   L <- global$t1 - 0.5*sum(Sinv*global$Omega) - 0.5*crossprod(global$beta)/vbeta0
   g <- numeric(d)
   S1 <- -global$beta/vbeta0
-  Xibeta = X[[i]] %*% global$beta
 
   if (r == 1){
     for (i in 1:n){
       tbi = ttheta[i]
+      Xibeta = X[[i]] %*% global$beta
       Lambdai = 1/(global$Omega + crossprod(ZH[[i]], Z[[i]]))
       Lit = sqrt(Lambdai)
       lambda_i = Lambdai * (Zg[i] - crossprod(ZH[[i]], Xibeta))
@@ -31,6 +31,7 @@ Lgrad_RVB1 <- function(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, n, p, r, d)
   else{
     for (i in 1:n){
       tbi <- ttheta[((i - 1)*r+1):(i*r)]
+      Xibeta = X[[i]] %*% global$beta
       Lambdai <- (global$Omega + ZH[[i]] %*% Z[[i]]) %>% chol() %>% chol2inv()
       Lit <- chol(Lambdai)
       lambda_i <- Lambdai %*% (Zg[i,] - ZH[[i]] %*% Xibeta)
@@ -39,22 +40,21 @@ Lgrad_RVB1 <- function(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, n, p, r, d)
       ai = -global$Omega %*% bi
       L = L + crossprod(y[[i]], etai) - sum(h0(model, etai, m[i])) + .logdet(Lit) +
         0.5*crossprod(bi, ai)
-      gi = y[[i]] + h1(model, etai, m[i])
+      gi = y[[i]] - h1(model, etai, m[i])
       ai = ai + crossprod(Z[[i]], gi)
       ai = Lit %*% ai
       ai_tbi <- tcrossprod(ai, tbi)
-      ai_tbi[upper.tri(ai_tbi)] = 0
-      sym_ai_tbi = ai_tbi + t(ai_tbi) - diag(ai_tbi)*diag(nrow(ai_tbi))
-      Sinv = Sinv + crossprod(Lit, sym_ai_tbi %*% Lit)
+      ai_tbi[upper.tri(ai_tbi)] = ai_tbi[lower.tri(ai_tbi)] # Make symmetric
+      Sinv = Sinv + crossprod(Lit, ai_tbi %*% Lit)
       g[((i - 1)*r + 1):(i*r)] = ai
       ai = crossprod(Lit, ai)
       S1 = S1 + crossprod(X[[i]], gi - crossprod(ZH[[i]], ai))
       ci = tcrossprod(ai, lambda_i)
       Sinv = Sinv + ci + t(ci) + Lambdai + tcrossprod(bi)
     }
-    g[(n*r + 1): (n*r + p)] = S1
+    g[(n*r + 1):(n*r + p)] = S1
     A = -get_lower_tri_vector(Sinv %*% global$W)
-    A[.diag_locs(r)] = A[.diag_locs(r)] * diag(global$W) + global$rseq
+    A[.diag_locs(r)] = A[.diag_locs(r)] * diag(global$W %>% as.matrix()) + global$rseq
     g[(n*r + p + 1):d] = A
   }
   return(list(L = (L %>% drop()), g = g))
--- R/logpy_RVB1.R
+++ R/logpy_RVB1.R
@@ -1,11 +1,11 @@
 logpy_RVB1 <- function(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, m, n, p, r){
   global <- global_var_components(ttheta, n, p, r)
-  Xibeta = X[[i]] %*% global$beta
 
   if (r == 1){
     L = global$t1 - 0.5 * Sinv * global$Omega -
       0.5 * crossprod(global$beta)/vbeta0
     for (i in 1:n){
+      Xibeta = X[[i]] %*% global$beta
       Lambdai = 1/(global$Omega + crossprod(ZH[[i]], Z[[i]]))
       Lit = sqrt(Lambdai)
       bi = Lambdai * (Zg[i] - crossprod(ZH[[i]], Xibeta))
@@ -19,12 +19,13 @@ logpy_RVB1 <- function(ttheta, y, X, Z, Zg, ZH, vbeta0, Sinv, model, m, n, p, r)
     L = global$t1 - 0.5*sum(Sinv*global$Omega) -
       0.5*crossprod(global$beta)/vbeta0
     for (i in 1:n){
+      Xibeta = X[[i]] %*% global$beta
       Lambdai = (global$Omega + ZH[[i]] %*% Z[[i]]) %>% chol() %>% chol2inv()
       Lit = chol(Lambdai)
       bi = Lambdai %*% (Zg[i,] - (ZH[[i]] %*% Xibeta))
       bi = bi + crossprod(Lit, ttheta[((i - 1)*r+1):(i*r)])
       etai = Xibeta + Z[[i]] %*% bi
-      L = L + crossprod(y[i], etai) - sum(h0(model, etai, m[i])) + .logdet(Lit) -
+      L = L + crossprod(y[[i]], etai) - sum(h0(model, etai, m[i])) + .logdet(Lit) -
         0.5*crossprod(bi, global$Omega %*% bi)
     }
   }
