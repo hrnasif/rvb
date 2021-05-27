df6f1e4e77079483cb92ade49a76e5a155a1a92b
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-05-27 03:22:43 -0700

    transform Zi

---
+++ R/transformZi.R
@@ -0,0 +1,16 @@
+#' Transform Zi for RVB2
+#'
+#' @param Zi A vector or matrix
+#'
+#' @return (Zi^T Zi)^{-1} Zi
+#'
+#'@keywords internal
+.transformZi <- function(Zi){
+  if(ncol(Zi) == 1){
+    Zi = Zi/crossprod(Zi)
+  }
+  else{
+    Zi = solve(crossprod(Zi), t(Zi))
+  }
+  return(Zi)
+}
