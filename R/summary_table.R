54e753ea07f8d08f76ecef38e948d3a80b3bd66e
Hassan Nasif <20205159+hrnasif@users.noreply.github.com>
2021-06-04 17:28:24 -0700

    make summary table simpler

--- R/summary_table.R
+++ R/summary_table.R
@@ -1,43 +1,54 @@
 summary_table <- function(RVB1, RVB2, INLA, stanfit, n, p, r){
-  summary_matrix <- matrix(0, nrow = (p + 2*r), ncol = 4)
+  summary_matrix <- matrix(0, nrow = (p + 2), ncol = 4)
 
   # Row and Column headers
   colnames(summary_matrix) <- c("RVB1", "RVB2", "INLA", "MCMC")
+  rownames(summary_matrix) <- c(paste0("beta_", 1:p),
+                                "time",
+                                "widehat{mathcal{L}}")
+
+  mubeta = numeric(4*p)
+  sdbeta = numeric(4*p)
+  time = numeric(4)
+  LB = numeric(4)
 
   # RVB1 metrics
-  mubeta_RVB1 <- round(RVB1$mu[(n*r + 1):p], 3)
-  Cbeta_RVB1 <- RVB1$C[(n*r + 1):p, (n*r + 1):p]
-  sdbeta_RVB1 <- Cbeta_RVB1 %>% tcrossprod() %>% diag() %>% sqrt() %>% round(3)
-  time_RVB1 <- as.difftime(RVB1$dur, unit = "secs") %>% as.numeric()
-  L_RVB1 <- RVB1$LB[1,1]
+  mubeta[1:p] <- RVB1$mu[(n*r) + 1:p] %>% round(3)
+  Cbeta_RVB1 <- RVB1$C[(n*r) + 1:p, (n*r) + 1:p]
+  sdbeta[1:p] <- Cbeta_RVB1 %>% tcrossprod() %>% diag() %>% sqrt() %>% round(3)
+  time[1] <- as.difftime(RVB1$dur, unit = "secs") %>% as.numeric() %>% round(3)
+  LB[1] <- RVB1$LB[1,1] %>% round(3)
 
   # Same for RVB2
-  mubeta_RVB2 <- round(RVB2$mu[(n*r + 1):p], 3)
-  Cbeta_RVB2 <- RVB2$C[(n*r + 1):p, (n*r + 1):p]
-  sdbeta_RVB2 <- Cbeta_RVB2 %>% tcrossprod() %>% diag() %>% sqrt() %>% round(3)
-  time_RVB2 <- as.difftime(RVB2$dur, unit = "secs") %>% as.numeric()
-  L_RVB2 <- RVB2$LB[1,1]
+  mubeta[1:p + p] <- RVB2$mu[(n*r) + 1:p] %>% round(3)
+  Cbeta_RVB2 <- RVB2$C[(n*r) + 1:p, (n*r) + 1:p]
+  sdbeta[1:p + p] <- Cbeta_RVB2 %>% tcrossprod() %>% diag() %>% sqrt() %>% round(3)
+  time[2] <- as.difftime(RVB2$dur, unit = "secs") %>% as.numeric() %>% round(3)
+  LB[2] <- RVB2$LB[1,1] %>% round(3)
 
   # INLA
   inla_sum <- summary(INLA)
-  mubeta_inla <- inla_sum$fixed[,1] %>% as.numeric() %>% round(3)
-  sdbeta_inla <- inla_sum$fixed[,2] %>% as.numeric() %>% round(3)
-  time_inla <- stringr::str_sub(inla_sum$cpu.used, -4, -1) %>% as.numeric()
+  mubeta[1:p + 2*p] <- inla_sum$fixed[,1] %>% as.numeric() %>% round(3)
+  sdbeta[1:p + 2*p] <- inla_sum$fixed[,2] %>% as.numeric() %>% round(3)
+  time[3] <- stringr::str_sub(inla_sum$cpu.used, -4, -1) %>% as.numeric()
+  LB[3] = NA
 
   #Stan
+  stan_sum <- rstan::summary(stanfit)
+  mubeta[1:p + 3*p] <- stan_sum$summary[1:p,1] %>% as.numeric() %>% round(3)
+  sdbeta[1:p + 3*p] <- stan_sum$summary[1:p,2] %>% as.numeric() %>% round(3)
+  time[4]<- rstan::get_elapsed_time(stanfit) %>% rowSums() %>% mean() %>% round(3)
+  LB[4] = NA
 
+  summary_matrix[1:p,] <- paste0(mubeta, " pm ", sdbeta)
+  summary_matrix[p+1,] <- time
+  summary_matrix[p+2,] <- LB
 
-  summary_matrix[1:p,1] <-
+  return(xtable::xtable(summary_matrix, digits = 3))
 
-  if(r > 1){
-    rownames(summary_matrix) <- c(paste0("$\\beta_", 1:p, "$"),
-                                  paste0("$\\sigma_", 1:r, "$"),
-                                  "$\\rho$", "time", "$\\widehat{\\mathcal{L}}$")
-  } else{
-    rownames(summary_matrix) <- c(paste0("$\\beta_", 1:p, "$"),
-                                  paste0("$\\sigma_", 1:r, "$"),
-                                  "time", "$\\widehat{\\mathcal{L}}$")
-  }
-
-
+  # if(r > 1){
+  #   rownames(summary_matrix) <- c(paste0("$\\beta_", 1:p, "$"),
+  #                                paste0("$\\sigma_", 1:r, "$"),
+  #                                "$\\rho$", "time", "$\\widehat{\\mathcal{L}}$")
+  # } else{}
 }
